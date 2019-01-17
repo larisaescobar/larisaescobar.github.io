6036b0f7935d073a2e678498c43cdcc200425ff9
larisaescobar <larisa@mewssystems.com>
2019-01-17 22:08:13 +0100

    Updated coming soon message

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="flex-container">
-          <div style={{ maxWidth: 450 }}>
-            <img id="main-img" src="/flowers.jpg" alt="flowers" />
-          </div>
-          <div style={{ marginLeft: 5, maxWidth: 400 }}>
-            <h1>hello world</h1>
-            <AboutMe />
-            <Contact />
-          </div>
+          <AboutMe />
         </div>
-        <span>open your eyes</span>
       </div>
     );
   }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,14 +4,66 @@ class AboutMe extends Component {
   render() {
     return (
       <div>
-        <p>
-          i'm larisa, a <strong>ui designer wannabe</strong> currently working
-          as a ui developer based in beautiful prague. i think react is pretty
-          cool and all.
-        </p>
-        <p>
-          simplicity, flowers and symmetry make me smile. art is everything.
-        </p>
+        <h1>Hello there!</h1>
+        <p>Thanks for visiting, i'll be back soon :)</p>
+        <div class="svgcontainer">
+          <svg
+            class="svgspin"
+            width="163"
+            height="161"
+            viewBox="0 0 163 161"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="2.76538"
+              y="83.7593"
+              width="118.454"
+              height="108.095"
+              rx="12.4445"
+              transform="rotate(-45 2.76538 83.7593)"
+              fill="#FF7676"
+              fill-opacity="0.64"
+            />
+            <rect
+              x="37.5805"
+              y="39.0425"
+              width="43.7863"
+              height="43.7863"
+              transform="rotate(-45 37.5805 39.0425)"
+              stroke="#333333"
+              stroke-width="2.30454"
+            />
+            <rect
+              x="37.5805"
+              y="113.249"
+              width="43.7863"
+              height="43.7863"
+              transform="rotate(-45 37.5805 113.249)"
+              stroke="#333333"
+              stroke-width="2.30454"
+            />
+            <rect
+              x="1.62956"
+              y="76.3765"
+              width="43.7863"
+              height="43.7863"
+              transform="rotate(-45 1.62956 76.3765)"
+              stroke="#333333"
+              stroke-width="2.30454"
+            />
+            <rect
+              x="75.3752"
+              y="76.3765"
+              width="43.7863"
+              height="43.7863"
+              transform="rotate(-45 75.3752 76.3765)"
+              stroke="#333333"
+              stroke-width="2.30454"
+            />
+          </svg>
+        </div>
+        <p class="black-background">Get in touch: larisaesco@gmail.com</p>
       </div>
     );
   }

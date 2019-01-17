8495ca374fe2c212ba7f49dd155241f6209cef39
larisaescobar <larisa@mewssystems.com>
2019-01-17 20:23:38 +0100

    Updated pages

---
+++ precache-manifest.5adca8ea8bc316ccf0ec5f9e4e73bb6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a1b22788a8e0bab95ec",
+    "url": "/static/js/main.6a1b2278.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "6a1b22788a8e0bab95ec",
+    "url": "/static/css/main.38fd1944.chunk.css"
+  },
+  {
+    "revision": "41f8c5366213304b71480bea7571b069",
+    "url": "/index.html"
+  }
+];

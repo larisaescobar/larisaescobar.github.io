b36eb2342801baa29470e100363b47575afc643b
larisaescobar <larisa@mewssystems.com>
2019-01-24 19:54:20 +0100

    Updated text and style for temporary site

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,67 +5,53 @@ class AboutMe extends Component {
     return (
       <div>
         <h1>Hello there!</h1>
-        <p>
-          Thanks for visiting, I'm working on a new design, coming up soon :)
-        </p>
-        <div class="svgcontainer">
-          <svg
-            class="svgspin"
-            width="163"
-            height="161"
-            viewBox="0 0 163 161"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="2.76538"
-              y="83.7593"
-              width="118.454"
-              height="108.095"
-              rx="12.4445"
-              transform="rotate(-45 2.76538 83.7593)"
-              fill="#FF7676"
-              fill-opacity="0.64"
-            />
-            <rect
-              x="37.5805"
-              y="39.0425"
-              width="43.7863"
-              height="43.7863"
-              transform="rotate(-45 37.5805 39.0425)"
-              stroke="#333333"
-              stroke-width="2.30454"
-            />
-            <rect
-              x="37.5805"
-              y="113.249"
-              width="43.7863"
-              height="43.7863"
-              transform="rotate(-45 37.5805 113.249)"
-              stroke="#333333"
-              stroke-width="2.30454"
-            />
-            <rect
-              x="1.62956"
-              y="76.3765"
-              width="43.7863"
-              height="43.7863"
-              transform="rotate(-45 1.62956 76.3765)"
-              stroke="#333333"
-              stroke-width="2.30454"
-            />
-            <rect
-              x="75.3752"
-              y="76.3765"
-              width="43.7863"
-              height="43.7863"
-              transform="rotate(-45 75.3752 76.3765)"
-              stroke="#333333"
-              stroke-width="2.30454"
-            />
-          </svg>
-        </div>
-        <p class="black-background">Get in touch: larisaesco@gmail.com</p>
+        <h3>Thanks for visiting my site.</h3>
+        <h4>
+          I’m currently working on a new design, you can{" "}
+          <span className="yellowBlock">
+            <a className="yellowAnchor" href="mailto:larisaesco@gmail.com">
+              email me
+            </a>
+          </span>{" "}
+          or check out my{" "}
+          <span className="yellowBlock">
+            <a
+              className="yellowAnchor"
+              href="https://www.instagram.com/larinena/"
+              target="_blank"
+            >
+              instagram.
+            </a>
+          </span>
+        </h4>
+        <svg
+          class="fixed"
+          width="350"
+          height="200"
+          viewBox="0 0 450 256"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M445.084 272C448.305 256.842 450 241.119 450 225C450 100.736 349.264 0 225 0C100.736 0 0 100.736 0 225C0 241.119 1.69494 256.842 4.91631 272H445.084Z"
+            fill="url(#paint0_linear)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="225"
+              y1="0"
+              x2="225"
+              y2="450"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#FBB03B" />
+              <stop offset="0.484968" stop-color="#FEC8AB" />
+            </linearGradient>
+          </defs>
+        </svg>
       </div>
     );
   }

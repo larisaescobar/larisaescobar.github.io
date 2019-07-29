0213956085b5ec1d68c01de63188dfafb9504ae4
Larisa Escobar <larisaescobar@Larisas-MBP.localdomain>
2019-07-29 16:28:31 +0200

    Updated aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,15 +4,16 @@ class AboutMe extends Component {
   render() {
     return (
       <div>
-        <h1>Monica Larisa</h1>
+        <h1>Larisa Escobar</h1>
         <h3>I code and design user interfaces.<br></br>
         I like reactjs a lot. <br></br>
+        Prague based.  <br></br>
           Pretty dope overall. 
         </h3>
-        <h3><a href="https://github.com/larisaescobar/" target="_blank">github</a></h3>
-        <h3><a href="https://dribbble.com/larisaescobar" target="_blank">dribbble</a></h3>
-        <h3><a href="https://www.instagram.com/larinena/" target="_blank">instagram</a></h3>
-        <h3><a href="mailto:larisaesco@gmail.com?subject=Hola Larisa!" target="_blank">email</a></h3>
+        <h3><a href="https://github.com/larisaescobar/" target="_blank" rel="noopener noreferrer">github</a></h3>
+        <h3><a href="https://dribbble.com/larisaescobar" target="_blank" rel="noopener noreferrer">dribbble</a></h3>
+        <h3><a href="https://www.instagram.com/larinena/" target="_blank" rel="noopener noreferrer">instagram</a></h3>
+        <h3><a href="mailto:larisaesco@gmail.com?subject=Hola Larisa!" target="_blank" rel="noopener noreferrer">email</a></h3>
         
    
         

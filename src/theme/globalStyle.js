147792ae445ed3834e935dfe6eed64fd51e9bbf0
larisaescobar <larisa@mewssystems.com>
2020-05-20 13:35:30 +0200

    Deleted import on GlobalStyles

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('${props => props.theme.fontSource}');
-
   body {
     margin: 0;
     padding: 0;
